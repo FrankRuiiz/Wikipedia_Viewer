b54e62f8772f3d0a1f6728e19c8f452c97de76b0
Frank Ruiz <frank.ruiz.limon@gmail.com>
2016-05-30 17:07:55 -0700

    able to call wiki appi and display target info

--- wikiApp.js
+++ wikiApp.js
@@ -2,10 +2,45 @@
 var wikiApp = angular.module('wikiApp',[]);
 
 
-wikiApp.controller('wikiController', ['$scope', '$log', function($scope, $log) {
-    $scope.searchTerm = 'kobe bryant';
+wikiApp.controller('wikiController', ['$scope', '$log', '$searchWikiService', function($scope, $log, $searchWikiService) {
+    $scope.searchTerm = 'Kobe Bryant';
 
-    $log.log($scope.searchTerm);
+    $scope.wikiArray = [];
+
+    $scope.searchWiki = function() {
+        $scope.wikiArray = [];
+        $searchWikiService.get($scope.searchTerm)
+            .then(function(results) {
+                var searchResults = results.data.query.pages;
+                for (var prop in searchResults) {
+                    if(searchResults.hasOwnProperty(prop)) {
+                        $scope.wikiArray.push({
+                            title: searchResults[prop].title,
+                            extract: searchResults[prop].extract,
+                            page_id: searchResults[prop].pageid
+                        });
+                    }
+                }
+                console.log($scope.wikiArray);
+            });
+    }
 
 }]);
+
+
+wikiApp.factory('$searchWikiService', function($http) {
+
+    var baseUrl = 'http://en.wikipedia.org/w/api.php?',
+        queryString = 'action=query&format=json&prop=extracts&generator=search&exlimit=max&exintro=1&explaintext=1&gsrsearch=',
+        JSON_callback = '&callback=JSON_CALLBACK';
+
+    var wikiService = {
+        get: function(searchTerm) {
+            return $http.jsonp( baseUrl + queryString + searchTerm + JSON_callback );
+        }
+    };
+    return wikiService;
+});
+
+
 
