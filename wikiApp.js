6f62e99e1a76eb59c090cb494e0f051f7e7f2252
Frank Ruiz <frank.ruiz.limon@gmail.com>
2016-06-01 20:03:59 -0700

    added previewDescription function to parse the extract from wikipedia

--- wikiApp.js
+++ wikiApp.js
@@ -5,9 +5,8 @@ var wikiApp = angular.module('wikiApp',[]);
 wikiApp.controller('wikiController', ['$scope', '$log', '$searchWikiService', function($scope, $log, $searchWikiService) {
     var self = this;
     this.searchTerm = 'Kobe Bryant';
-
     this.wikiArray = [];
-
+    this.previewLen = 150;
     this.searchWiki = function() {
         this.wikiArray = [];
         $searchWikiService.get(this.searchTerm)
@@ -22,13 +21,23 @@ wikiApp.controller('wikiController', ['$scope', '$log', '$searchWikiService', fu
             if(results.hasOwnProperty(prop)) {
                 self.wikiArray.push({
                     title: results[prop].title,
-                    extract: results[prop].extract,
+                    extract: self.previewDescription(results[prop].extract, self.previewLen),
                     page_id: results[prop].pageid
                 });
             }
         }
-    }
+    };
 
+    this.previewDescription = function(str, num) {
+        var dots = '...';
+        if( num <= 3 ) {
+            str = str.slice(0, num) + dots;
+        }
+        else if (str.length > num ) {
+            str = str.slice(0, num-dots.length) + dots;
+        }
+        return str;
+    }
 }]);
 
 
